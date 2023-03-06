3b0bede32ef2cd1d23fbb77f9675045963f07548
reactor01 <kmnparts1@gmail.com>
2023-03-06 12:23:26 +0200

    Full

--- src/pages/contact.vue
+++ src/pages/contact.vue
@@ -4,7 +4,7 @@
       <VCardText />
       <VCardText>
         <p>Email: reactorsec@protonmail.com</p>
-        <p>Phone:🇧🇬 +359 884023112</p>
+        <p>Phone: 🇧🇬 +359 884023112</p>
       </VCardText>
     </VCard>
   </div>
